442ab6ed907abad1e7a0d467997c12d7a8104043
Aishwarya Bhargava <aishu51198@gmail.com>
2026-01-01 22:08:21 -0500

    Updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://portfolioai-4tpg.onrender.com",
+        "https://portfolio-ai-mauve.vercel.app/",
+        "http://127.0.0.1:8000",
     ],
     allow_credentials=True,
     allow_methods=["*"],          # <-- allows OPTIONS
